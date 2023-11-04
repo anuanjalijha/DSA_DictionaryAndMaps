09fac6e3944ca8832f3a7b89e816f697d87fad72
Anjali <anjalijanar@gmail.com>
2023-11-04 22:11:29 +0530

    Dictionary

---
+++ Dictionary3.py
@@ -0,0 +1,15 @@
+temp_dict = {}
+
+# Using update method
+temp_dict.update([("Sam", 92), ("Annie", 88)])
+# print(temp_dict)
+# print(temp_dict.keys())
+# temp_dict.update([("Sam", 95)])
+# print(temp_dict)
+
+temp_dict["Harry"] = 82
+print(temp_dict)
+
+temp_dict.clear()
+
+print(temp_dict)
